f26dfb6e731d27f023311d4a483ca74cff7cfaf5
minghc11 <minghui.cai.21@ucl.ac.uk>
2024-03-09 20:26:47 +0000

    remove default from title in notes model

--- oxytocin/oxytocin/models.py
+++ oxytocin/oxytocin/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Notes(models.Model):
-    title = models.CharField(max_length=100, default='Untitled')
+    title = models.CharField(max_length=100)
     content = models.TextField()
     date_last_modified = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
