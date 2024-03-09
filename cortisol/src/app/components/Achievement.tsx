12b4e1317b5d26c0aaa05da6f2bf4db00ce8c5d5
minghc11 <minghui.cai.21@ucl.ac.uk>
2024-03-09 17:02:44 +0000

    add achievement section

---
+++ cortisol/src/app/components/Achievement.tsx
@@ -0,0 +1,25 @@
+import {Box} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Badge from "@/app/components/Badge";
+import AndroidIcon from "@mui/icons-material/Android";
+import AppleIcon from '@mui/icons-material/Apple';
+import ApiIcon from '@mui/icons-material/Api';
+
+export default function Achievement() {
+    return (
+        <Box border={1} borderRadius={10} borderColor="primary.main" p={2}>
+            <Typography variant='body2'>
+                Achievements:
+            </Typography>
+            <Badge>
+                <AndroidIcon/>
+            </Badge>
+            <Badge>
+                <AppleIcon/>
+            </Badge>
+            <Badge>
+                <ApiIcon/>
+            </Badge>
+        </Box>
+    )
+}
