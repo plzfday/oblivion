e8d004b0aa14f9567aeff8665a6c191b255e7107
YoongXin <zcabyx0@ucl.ac.uk>
2024-03-09 16:43:33 +0000

    Update

--- cortisol/src/app/profile/page.tsx
+++ cortisol/src/app/profile/page.tsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar';
 
 export default function Note() {
   return (
-    <Sidebar header={'User Profile'}>
+    <Sidebar header={'User Profile'} sideList={['Home', 'Notes', 'Flashcards', 'Profile']}>
         <DateCalendarValue/>
     </Sidebar>
   );
