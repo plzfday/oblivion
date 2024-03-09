2cf4f5ebd0543fd58bc43e128a3b16a152f6d323
Dongyeon Park <26868715+plzfday@users.noreply.github.com>
2024-03-09 15:06:30 +0000

    fix: :bug: Reorganise the components

--- cortisol/src/app/notes/page.tsx
+++ cortisol/src/app/notes/page.tsx
@@ -1,7 +1,11 @@
-import Sidebar from "../components/Sidebar";
+import { Box } from '@mui/material';
+import Sidebar from '../components/Sidebar';
+import Navigation from '../components/Navigation';
 
 export default function Note() {
   return (
-    <Sidebar header={'Today I Learned'}></Sidebar>
+    <Sidebar header={'Today I Learned'}>
+      <Navigation />
+    </Sidebar>
   );
 }
