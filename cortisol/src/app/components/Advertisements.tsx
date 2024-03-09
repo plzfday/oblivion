e28ccff8e01e676d6be731739cb1e09ea4f607f5
minghc11 <minghui.cai.21@ucl.ac.uk>
2024-03-09 16:31:57 +0000

    cleanup

--- cortisol/src/app/components/Advertisements.tsx
+++ cortisol/src/app/components/Advertisements.tsx
@@ -3,17 +3,15 @@ import AdvertisementCard from "@/app/components/AdvertisementCard";
 import Box from "@mui/material/Box";
 import {Grid, Stack} from "@mui/material";
 
-export default function Advertisements () {
+export default function Advertisements() {
     return (
         <Grid container spacing={2} columns={16}>
             <Grid item xs={8}>
-                <AdvertisementCard title='Ad' content='Some ads' />
+                <AdvertisementCard title='Ad' content='Some ads'/>
             </Grid>
             <Grid item xs={8}>
-            <AdvertisementCard title='Ad2' content='Some ads' />
-        </Grid>
-
-
+                <AdvertisementCard title='Ad2' content='Some ads'/>
+            </Grid>
         </Grid>
 
     );
