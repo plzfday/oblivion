5c9600b9c5c7c6560117ca4d8f82d419974a84b2
YoongXin <zcabyx0@ucl.ac.uk>
2024-03-09 15:01:12 +0000

    Added props for sidebar

--- cortisol/src/app/notes/page.tsx
+++ cortisol/src/app/notes/page.tsx
@@ -4,7 +4,7 @@ import Navigation from '../components/Navigation';
 
 export default function Note() {
   return (
-    <Sidebar header={'Today I Learned'}>
+    <Sidebar header={'Today I Learned'} sideList={['2024-02-01', '2024-01-31', '2024-01-30', '2024-01-29']}>
       <Navigation />
     </Sidebar>
   );
