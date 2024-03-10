13504649a27821149b80e77e8c33e069df1d8d7c
youngwoohome <youngwoo5662@gmail.com>
2024-03-10 07:32:58 +0000

    add : statistics

--- oxytocin/notes/admin.py
+++ oxytocin/notes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Notes
+from .models import Notes, Category
 
 # Register your models here.
 admin.site.register(Notes)
+admin.site.register(Category)
--- oxytocin/notes/urls.py
+++ oxytocin/notes/urls.py
@@ -1,9 +1,10 @@
 
 
 from django.urls import path
-from .views import NoteList, NoteDetail
+from .views import NoteList, NoteDetail, CalculateStats
 
 urlpatterns=[
     path("", NoteList.as_view()),
     path("<int:pk>", NoteDetail.as_view()),
+    path("stat", CalculateStats.as_view())
 ]
